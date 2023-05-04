e55339575125088666162189a0fe6aa7ebb8a003
YongPang <yongpang99.22@ichat.sp.edu.sg>
2023-05-04 15:42:51 +0800

    first commit

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,16 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    bmi = weight/(height*height)
+    print("BMI = " +str(bmi))
+    if bmi < 18.5:
+        print("Under Weight")
+        return -1
+    elif 18.5 <= bmi <= 25.0:
+        print("Normal Weight")
+        return 0
+    else:
+        print("Over Weight")
+        return 1
